8ee9847f03292a26055ef40d28fed76c9138f714
Douglas Beck <dbeck@douglasbeck.com>
2011-05-02 11:11:57 -0400

    adding a single page for provost_award

---
+++ single-provost_award.php
@@ -0,0 +1,12 @@
+<?php
+/* 
+	prevents seeing a blank page for the custom post type
+	page is accessible through the search
+*/
+$post_url = get_post_meta( $post->ID, 'provost_award_url', true );
+if(empty($post_url)){
+	header("Location: " . site_url());
+} else {
+	header("Location: " . $post_url);
+}
+die();
