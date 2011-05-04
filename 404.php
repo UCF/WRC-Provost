526062e4ff19741490093c92a5f6a114b770cf18
Douglas Beck <dbeck@douglasbeck.com>
2011-05-04 10:27:09 -0400

    provost: updating 404 to pull content from a cms page

--- 404.php
+++ 404.php
@@ -3,7 +3,21 @@
 <?php get_header(); the_post();?>
 	
 	<div class="page-content" id="page-not-found">
-		<?php thematic_404()?>
+		<?php 
+			$page = get_page_by_title('404');
+			if($page){
+				$content = $page->post_content;
+				$content = apply_filters('the_content', $content);
+				$content = str_replace(']]>', ']]>', $content);
+				echo $content;
+			}
+		?>
+		<form id="error404-searchform" method="get" action="<?php bloginfo('url') ?>/">
+			<div>
+				<input id="error404-s" name="s" type="text" value="<?php echo esc_html(stripslashes(get_query_var('s'))) ?>" size="40" />
+				<input id="error404-searchsubmit" name="searchsubmit" type="submit" value="<?php _e('Find', 'thematic') ?>" />
+			</div>
+		</form>
 	</div>
 
 <?php get_footer();?>
